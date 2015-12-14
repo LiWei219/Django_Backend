ff36eb4d257495751565ad9a17a60f0e58e63a4d
liyangguang1993 <308159288@qq.com>
2015-12-14 11:22:08 +0800

    add redis api

--- server/TrafficMonitor/views_dushboard_resources.py
+++ server/TrafficMonitor/views_dushboard_resources.py
@@ -1,14 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+import redis
 # Create your views here.
 
 class RealtimeApplicationThroughput(APIView):
 	def get(self, request, format=None):
-		#
-		#include method here
-		#
-		return Response({'Response':'RealtimeApplicationThroughput'})
+		length = 10
+		data = []
+		r = redis.Redis(host='166.111.143.200', port=6379, db=0)
+		#line1 = r.get('throuput')
+		#line2 = r.get('countPacket')
+		line1 = '1/2/3/4/5/6/7/8/9/0'
+		line2 = '0/9/8/7/6/5/4/3/2/1'
+		array1 = line1.split('/')
+		array2 = line2.split('/')
+
+		for i in range(length):
+			body = {}
+			body['throuput'] = array1[i]
+			body['countPacket'] = array2[i]
+			data.append(body)
+
+		return Response(data)
 
 class RealtimeApplicationDataPacket(APIView):
 	def get(self, request, format=None):
